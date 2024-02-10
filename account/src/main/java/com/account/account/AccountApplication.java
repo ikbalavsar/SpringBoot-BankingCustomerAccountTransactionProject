e5b38493697b21d10327760a9df5b13926249916
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2024-02-10 21:49:52 +0300

    Creating Project - Prepare Models with Kotlin Class

---
+++ account/src/main/java/com/account/account/AccountApplication.java
@@ -0,0 +1,19 @@
+package com.account.account;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
