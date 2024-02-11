07ab2d34e2d29117f365570fb5c0d10dede4cbe0
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2024-02-11 21:13:54 +0300

    Account Service is done

--- account/src/main/java/com/account/account/service/TransactionService.java
+++ account/src/main/java/com/account/account/service/TransactionService.java
@@ -1,13 +1,27 @@
 package com.account.account.service;
 
+import com.account.account.model.Account;
+import com.account.account.model.Transaction;
 import com.account.account.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class TransactionService {
+    private Logger logger = LoggerFactory.getLogger(TransactionService.class);
     private final TransactionRepository transactionRepository;
 
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
+
+    protected Transaction initiateMoney(final Account account, BigDecimal amount){
+        return transactionRepository.save(
+                new Transaction(amount,account)
+        );
+    }
+
 }
